4d2e208cf912c0d705a1ea5e701cf773459b7caf
victor-VN <v1ctoroliverianasc@gmail.com>
2020-08-01 18:25:05 -0300

    First Commit

---
+++ SimpleSOAPExample/src/org/arpit/javapostsforlearning/webservices/HelloWorld.java
@@ -0,0 +1,8 @@
+package org.arpit.javapostsforlearning.webservices;
+
+public class HelloWorld {
+
+	public String sayHelloWorld(String name) {
+		return "Hello World from " + name;
+	}
+}
---
+++ SimpleSOAPExampleClient/src/org/arpit/javapostsforlearning/webservices/HelloWorldProxy.java
@@ -0,0 +1,54 @@
+package org.arpit.javapostsforlearning.webservices;
+
+public class HelloWorldProxy implements org.arpit.javapostsforlearning.webservices.HelloWorld {
+  private String _endpoint = null;
+  private org.arpit.javapostsforlearning.webservices.HelloWorld helloWorld = null;
+  
+  public HelloWorldProxy() {
+    _initHelloWorldProxy();
+  }
+  
+  public HelloWorldProxy(String endpoint) {
+    _endpoint = endpoint;
+    _initHelloWorldProxy();
+  }
+  
+  private void _initHelloWorldProxy() {
+    try {
+      helloWorld = (new org.arpit.javapostsforlearning.webservices.HelloWorldServiceLocator()).getHelloWorld();
+      if (helloWorld != null) {
+        if (_endpoint != null)
+          ((javax.xml.rpc.Stub)helloWorld)._setProperty("javax.xml.rpc.service.endpoint.address", _endpoint);
+        else
+          _endpoint = (String)((javax.xml.rpc.Stub)helloWorld)._getProperty("javax.xml.rpc.service.endpoint.address");
+      }
+      
+    }
+    catch (javax.xml.rpc.ServiceException serviceException) {}
+  }
+  
+  public String getEndpoint() {
+    return _endpoint;
+  }
+  
+  public void setEndpoint(String endpoint) {
+    _endpoint = endpoint;
+    if (helloWorld != null)
+      ((javax.xml.rpc.Stub)helloWorld)._setProperty("javax.xml.rpc.service.endpoint.address", _endpoint);
+    
+  }
+  
+  public org.arpit.javapostsforlearning.webservices.HelloWorld getHelloWorld() {
+    if (helloWorld == null)
+      _initHelloWorldProxy();
+    return helloWorld;
+  }
+  
+  public java.lang.String sayHelloWorld(java.lang.String name) throws java.rmi.RemoteException{
+    if (helloWorld == null)
+      _initHelloWorldProxy();
+    return helloWorld.sayHelloWorld(name);
+  }
+  
+  
+}
